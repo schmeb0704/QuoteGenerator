866c714d0ddc371789ee0537c4d65732740ef181
schmeb0704 <113418470+schmeb0704@users.noreply.github.com>
2022-11-27 05:32:11 +0000

    made tweeting the quote work

--- src/components/quoteBox.jsx
+++ src/components/quoteBox.jsx
@@ -3,17 +3,25 @@ import React from "react"
 export default (props) =>{
   const quoteList = props.list
   const newList = quoteList.map(quote => {
+    const href = `https://twitter.com/intent/tweet?text="${quote.text}" - ${quote.author}`
     return( 
     <>
       <h1 key={Date.now()} id="text" className="quote-text"><span className="material-symbols-outlined">format_quote</span>{quote.text}</h1>
       <p id="author">{quote.author}</p>
+      <a id="tweet-quote" href={href}><i className='bx bxl-twitter'></i></a>
     </>)
     
   })
 
+  const toDisplay = newList[props.num]
+  const wtf = quoteList[props.num]
+
+  
+
   return(
     <div>
-      {newList[props.num]}
+      {toDisplay}
+     
     </div>
   )
 }
