ef1f088a5289f77d03bb31b0ff0a62334cd85a4f
schmeb0704 <113418470+schmeb0704@users.noreply.github.com>
2022-11-27 03:50:17 +0000

    used fetch and made it work

--- src/components/quoteBox.jsx
+++ src/components/quoteBox.jsx
@@ -1,7 +1,27 @@
+import React from "react"
+
 export default (props) =>{
+  const [quoteList, setQuoteList] = React.useState([])
+  let ranNum = Math.floor(Math.random() * quoteList.length)
+
+  React.useEffect(() => {
+    fetch("https://type.fit/api/quotes")
+    .then(response => response.json())
+    .then(data => setQuoteList(data))
+  }, [])
+
+  const newList = quoteList.map(quote => {
+    return( 
+    <>
+      <h1 id={Date.now()} className="quote-text">{quote.text}</h1>
+      <p className="author">{quote.author}</p>
+    </>)
+    
+  })
+
   return(
     <div>
-      Quote Here
+      {newList[ranNum]}
     </div>
   )
 }
